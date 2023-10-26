d4f1d24c8d067cc6b9bb8b5ac15df4e2f46c5912
Steve Yu <steveyuowo@gmail.com>
2023-10-26 12:31:08 +0800

    fix: update examples

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,13 @@ import "./styles/index.scss";
 </script>
 
 <template>
-  <div class="container">
+  <div :style="{ display: 'flex', gap: '10px'}">
     <button
       @click="
         toast({
-          type: 'loading',
-          duration: 9000000
-        })
-      "
-    >
-      Loading
-    </button>
-    <button
-      @click="
-        toast({
+          message: `Here's your toast`,
           type: 'success',
-          autoClose:false
+          autoClose: false
         })
       "
     >
@@ -28,6 +19,17 @@ import "./styles/index.scss";
     <button
       @click="
         toast({
+          message: `Here's your toast`,
+          type: 'loading',
+        })
+      "
+    >
+      Loading
+    </button>
+    <button
+      @click="
+        toast({
+          message: `Here's your toast`,
           type: 'error',
         })
       "
@@ -38,32 +40,3 @@ import "./styles/index.scss";
   <Toaster />
 </template>
 
-<style lang="scss" scoped>
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-}
-
-body {
-  height: 100vh;
-  width: 100vw;
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-.container {
-  display: flex;
-  gap: 20px;
-  button {
-    color: #101010;
-    padding: 10px 20px;
-    background: white;
-    border-radius: 8px;
-    border: none;
-    box-shadow: 2px 2px 2px 0 rgba(0, 0, 0, 0.2);
-  }
-}
-</style>
